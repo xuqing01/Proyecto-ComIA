7e2c5fa4babdeab9d79732ca05675b3415568edf
xuqing01 <xuqing.li2006@gmail.com>
2026-06-24 18:43:37 -0400

    Dependencias agregadas Main temporal con la base de conectar la base de datos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 
 public class Main {
     public static void main(String[] args) {
-        
+        String connectionString = "mongodb://localhost:27017";
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            MongoDatabase database = mongoClient.getDatabase("Comida");
+
+            // Metodos aca
+
+        } catch (Exception e) {
+            System.err.println("Error al conectar o interactuar con MongoDB: " + e.getMessage());
+        }
     }
 }
